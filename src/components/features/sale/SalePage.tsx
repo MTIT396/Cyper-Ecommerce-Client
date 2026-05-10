70aa7894aa05d08517fde57f6011215c42b1bc77
MTIT396 <minhthienit39@gmail.com>
2026-05-10 17:07:50 +0700

    fix: refactor routes

--- src/components/features/sale/SalePage.tsx
+++ src/components/features/sale/SalePage.tsx
@@ -1,7 +1,17 @@
-import SalePage from '@/app/(main)/sale/page'
+'use client'
+import ProductsLayout from '@/components/features/catalog/ProductsLayout'
+import { useFilterProducts } from '@/hooks/useFilterProducts'
+export const SALE_PERCENT = 30
 
-const Page = () => {
-   return <SalePage />
+export default function SalePage() {
+   const { products, isLoading } = useFilterProducts()
+   const saleProducts =
+      products?.filter(
+         (p) => ((p.base_price - p.sale_price) / p.base_price) * 100 > SALE_PERCENT
+      ) || products
+   return (
+      <div className='px-2 py-10 sm:px-4'>
+         <ProductsLayout isLoading={isLoading} products={saleProducts}></ProductsLayout>
+      </div>
+   )
 }
-
-export default Page
