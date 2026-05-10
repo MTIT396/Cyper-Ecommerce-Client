e3f2c755bfb2c8c85d62dae55c900378f077c3a4
MTIT396 <minhthienit39@gmail.com>
2026-05-10 16:11:50 +0700

    fix: fix ssr render again

--- src/app/[...all]/page.tsx
+++ src/app/[...all]/page.tsx
@@ -1,6 +1,9 @@
-import NotFound from '@/components/features/notfound/NotFound'
-export const dynamic = 'force-dynamic'
+import dynamic from 'next/dynamic'
+
+const NotFoundPage = dynamic(() => import('@/components/features/notfound/NotFound'), {
+   ssr: false
+})
 
 export default function Page() {
-   return <NotFound />
+   return <NotFoundPage />
 }
