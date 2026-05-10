bc193635f3d378b385b5925e83d2dac2b58d3e4e
MTIT396 <minhthienit39@gmail.com>
2026-05-10 17:11:12 +0700

    fix: refactor routes

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,31 +1,7 @@
-'use client'
+import SearchPage from '@/components/features/search/SeachPage'
 
-import Container from '@/components/Container'
-import ProductsGrid from '@/components/features/catalog/ProductsGrid'
-import BodyContent from '@/components/shared/BodyContent'
-import { useFilterProducts } from '@/hooks/useFilterProducts'
-import { useSidebarToggle } from '@/hooks/useSidebarToggle'
-import FilterSidebar from '@/layouts/FilterSidebar'
-
-const SearchPage = () => {
-   const { products, isLoading } = useFilterProducts()
-   const { isShowSidebar, toggleSidebar } = useSidebarToggle()
-
-   return (
-      <Container>
-         <BodyContent>
-            <div className='mt-10 flex items-start gap-6'>
-               <FilterSidebar isShowSidebar={isShowSidebar} />
-               <ProductsGrid
-                  products={products}
-                  isLoading={isLoading}
-                  isShowSidebar={isShowSidebar}
-                  onShowSidebar={toggleSidebar}
-               />
-            </div>
-         </BodyContent>
-      </Container>
-   )
+const Page = () => {
+   return <SearchPage />
 }
 
-export default SearchPage
+export default Page
