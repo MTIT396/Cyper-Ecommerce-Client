5c035fa9f8c994c2f857cc8affe5151d8c6b90ea
MTIT396 <minhthienit39@gmail.com>
2026-05-10 16:03:52 +0700

    fix: add suspense wrapper

--- src/components/features/notfound/NotFound.tsx
+++ src/components/features/notfound/NotFound.tsx
@@ -2,11 +2,15 @@
 import FallbackExpired from '@/components/shared/FallbackExpired'
 import Footer from '@/layouts/Footer'
 import Header from '@/layouts/Header'
+import { Suspense } from 'react'
 
 export default function NotFound() {
    return (
       <>
-         <Header />
+         <Suspense fallback={null}>
+            <Header />
+         </Suspense>
+
          <FallbackExpired />
          <Footer />
       </>
